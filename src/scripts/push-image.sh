6dd4412aa5731c07816300a78b156270556540a9
Baudouin Herlicq <baudouin.herlicq@gopigment.com>
2022-11-04 11:13:36 +0100

    Add code to handle the push to GAR, needs to be tested

--- src/scripts/push-image.sh
+++ src/scripts/push-image.sh
@@ -5,11 +5,19 @@ PROJECT_ID="${!ORB_ENV_PROJECT_ID}"
 
 for tag_to_eval in "${DOCKER_TAGS[@]}"; do
     TAG=$(eval echo "$tag_to_eval")
-    docker push "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$TAG"
+    if [[ "$ORB_VAL_REGISTRY_URL" == *"docker.pkg.dev" ]]; then
+        docker push "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$TAG"
+    else
+        docker push "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_REPOSITORY_NAME/$ORB_VAL_IMAGE:$TAG"
+    fi
 done
 
 if [ -n "$ORB_VAL_DIGEST_PATH" ]; then
     mkdir -p "$(dirname "$ORB_VAL_DIGEST_PATH")"
     SAMPLE_FIRST=$(eval echo "${DOCKER_TAGS[0]}")
-    docker image inspect --format="{{index .RepoDigests 0}}" "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$SAMPLE_FIRST" > "$ORB_VAL_DIGEST_PATH"
+    if [[ "$ORB_VAL_REGISTRY_URL" == *"docker.pkg.dev" ]]; then
+        docker image inspect --format="{{index .RepoDigests 0}}" "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$SAMPLE_FIRST" > "$ORB_VAL_DIGEST_PATH"
+    else
+        docker image inspect --format="{{index .RepoDigests 0}}" "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_REPOSITORY_NAME/$ORB_VAL_IMAGE:$SAMPLE_FIRST" > "$ORB_VAL_DIGEST_PATH"
+    fi 
 fi
